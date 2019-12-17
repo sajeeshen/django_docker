5c2b26466310237b508e1c9a932c076d4f83e291
Sajeesh E Namboothiri <sajeesh.namboothiri@awok.com>
2019-12-17 19:04:19 +0530

    Added celery

--- app/app/settings.py
+++ app/app/settings.py
@@ -153,3 +153,10 @@ MEDIA_ROOT = '/vol/web/media'
 STATIC_ROOT = '/vol/web/static'
 
 STATIC_ROOT      =  os.path.join(BASE_DIR, "static") 
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
