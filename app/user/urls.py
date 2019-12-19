97f8b16a1b4a750a134e2973691554e13b67953d
Sajeesh E Namboothiri <sajeesh.namboothiri@awok.com>
2019-12-19 16:03:23 +0530

    Added user profile part

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,5 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserCreateViewSet, CreateTokenView
+from .views import ( UserCreateViewSet,
+                     CreateTokenView,
+                     ManageUserView )
 from django.urls import path, include
 
 app_name = 'user'
@@ -8,4 +10,5 @@ app_name = 'user'
 urlpatterns = [
     path('user/create/', UserCreateViewSet.as_view()),
     path('user/login/', CreateTokenView.as_view()),
+    path('user/me/', ManageUserView.as_view()),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from core.models import Profile
-from rest_framework import viewsets, generics
+from rest_framework import ( viewsets, generics,
+                            authentication, permissions)
 from .serializers import UserSerializer, AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
@@ -14,6 +15,7 @@ class UserCreateViewSet(generics.CreateAPIView):
     """ API endpoint that allows to create new user """
 
     def get_serializer_class(self):
+        """ Check the version and return the serializers """
         # if self.request.version == 'v1':
         #     return UserSerializer
         return UserSerializer
@@ -22,3 +24,15 @@ class CreateTokenView(ObtainAuthToken):
     """ Create a new auth token for users """
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """ Manage the authenticated user """
+    
+    serializer_class = UserSerializer
+    authentication_class = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """ Retrieve and return the user objects """
+        return self.request.user
